d42d9788650e11a0e468d929c5e68e8e223b56a1
litvinovl-digital <70130791+litvinovl-digital@users.noreply.github.com>
2021-03-12 00:36:31 +0300

    робонет

--- credit_bank/app/js/app.js
+++ credit_bank/app/js/app.js
@@ -10,6 +10,21 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+let tabs = document.querySelector('.tabs')
+let btns = tabs.querySelectorAll('.tabs__btn')
+let items = tabs.querySelectorAll('.tabs__item')
+
+function change(arr, i) {
+	arr.forEach( item => {
+		item.forEach( i => {i.classList.remove('active')})
+		item[i].classList.add('active')
+	})
+}
+
+for(let i = 0; i < btns.length; i++) {
+	btns[i].addEventListener('click', () => {
+		change([btns, items], i)
+	})
+}
 
 })
